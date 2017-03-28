314e555281b2fd38b40ff36c8fdfd993cf0de50b
MTT <v2top1@163.com>
2017-03-29 01:17:36 +0800

    sample code update

--- Objet-C-Language/Objet-C-Language/main.m
+++ Objet-C-Language/Objet-C-Language/main.m
@@ -53,10 +53,45 @@ int main(int argc, const char * argv[]) {
 //        
 //        Student tom2 ={"tom liu",25,170};
         
-        
+//        struct objc_class * class;
         
         
         Class clas ;
+        //任何一个OC对象的类 都需要两步来判断这个类是不是元类metaclass
+        NSObject *object = [[NSObject alloc] init];
+        Class class = [NSObject class];
+        MTTStudent * Kangkang = [[MTTStudent alloc] init];
+        
+       // 如果一个对象是类对象，那么object_getClass 可以得到这个对象的类的isa 指针是指向与其相对应的元类metaclass
+        BOOL yup = class_isMetaClass(object_getClass(class));
+        //NSObject的实例，那么object_getClass 可以得到这个对象的类是NSObject
+        BOOL nope = class_isMetaClass(object_getClass(object));
+        //如果一个对象是继承自NSObject的某一个类的实例，那么object_getClass 可以得到这个类
+        BOOL what = class_isMetaClass(object_getClass(Kangkang));
+      
+//        NSMutableArray *arr = [[NSMutableArray alloc] init];
+//        
+//        [arr addObject:[NSObject class]];
+//        [arr addObject:[NSValue class]];
+//        [arr addObject:[NSNumber class]];
+//        [arr addObject:[NSPredicate class]];
+//        [arr addObject:@"not a class object"];
+//        
+//        for (int i; i<[arr count]; i++) {
+//            id obj = [arr objectAtIndex:i];
+//            
+//            if(class_isMetaClass(object_getClass(obj)))
+//            {
+//                //do sth
+//                NSLog(@"Class: %@", obj);
+//            }
+//            else
+//            {
+//                NSLog(@"Instance: %@", obj);
+//            }
+//        }
+//
+    
         
         
         
